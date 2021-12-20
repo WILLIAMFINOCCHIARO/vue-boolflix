acc2989f94297de6596dcdc1a5332de7a6c09133
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-12-20 17:26:29 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from '../src/components/macro/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
